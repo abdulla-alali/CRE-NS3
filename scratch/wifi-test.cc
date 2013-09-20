15ddf43320b94c76ca986baa624209bb76a24bbf
Abdulla Al-Ali <al-ali.a@husky.neu.edu>
2013-09-20 14:25:05 -0400

    fix indentation and add a Rx callback for sink

--- scratch/wifi-test.cc
+++ scratch/wifi-test.cc
@@ -70,136 +70,150 @@ NS_LOG_COMPONENT_DEFINE ("WifiSimpleAdhoc");
 
 using namespace ns3;
 
-void ReceivePacket (Ptr<Socket> socket)
-{
-	NS_LOG_UNCOND ("Received one packet!");
+static void RxRcv(std::string context, Ptr<const Packet> p, const Address &address) {
+  Packet::EnablePrinting();
+  int bytes = 0;
+  if (p) {
+    bytes = p->GetSize();
+  }
+  NS_LOG_UNCOND ( Simulator::Now().GetSeconds() << context << " Received a packet and size: " << bytes);
 }
-
-static void GenerateTraffic (Ptr<Socket> socket, uint32_t pktSize,
-		uint32_t pktCount, Time pktInterval )
-{
-	if (pktCount > 0)
-	{
-		socket->Send (Create<Packet> (pktSize));
-		Simulator::Schedule (pktInterval, &GenerateTraffic,
-				socket, pktSize,pktCount-1, pktInterval);
-	}
-	else
-	{
-		socket->Close ();
-	}
-}
-
 
 int main (int argc, char *argv[])
 {
-	std::string phyMode ("ErpOfdmRate54Mbps");
-	bool verbose = false;
 
-	CommandLine cmd;
+  Packet::EnablePrinting();
+  std::string phyMode ("ErpOfdmRate54Mbps");
+  bool verbose = false;
 
-	cmd.AddValue ("phyMode", "Wifi Phy mode", phyMode);
-	cmd.AddValue ("verbose", "turn on all WifiNetDevice log components", verbose);
+  CommandLine cmd;
 
-	cmd.Parse (argc, argv);
+  cmd.AddValue ("phyMode", "Wifi Phy mode", phyMode);
+  cmd.AddValue ("verbose", "turn on all WifiNetDevice log components", verbose);
 
-	// disable fragmentation for frames below 2200 bytes
-	Config::SetDefault ("ns3::WifiRemoteStationManager::FragmentationThreshold", StringValue ("2200"));
-	// turn off RTS/CTS for frames below 2200 bytes
-	Config::SetDefault ("ns3::WifiRemoteStationManager::RtsCtsThreshold", StringValue ("2200"));
-	// Fix non-unicast data rate to be the same as that of unicast
-	Config::SetDefault ("ns3::WifiRemoteStationManager::NonUnicastMode",
-			StringValue (phyMode));
+  cmd.Parse (argc, argv);
 
-	NodeContainer c;
-	c.Create (2);
+  // disable fragmentation for frames below 2200 bytes
+  Config::SetDefault ("ns3::WifiRemoteStationManager::FragmentationThreshold", StringValue ("2200"));
+  // turn off RTS/CTS for frames below 2200 bytes
+  Config::SetDefault ("ns3::WifiRemoteStationManager::RtsCtsThreshold", StringValue ("2200"));
+  // Fix non-unicast data rate to be the same as that of unicast
+  Config::SetDefault ("ns3::WifiRemoteStationManager::NonUnicastMode",
+      StringValue (phyMode));
 
-	// The below set of helpers will help us to put together the wifi NICs we want
-	WifiHelper wifi;
-	if (verbose)
-	{
-		wifi.EnableLogComponents ();  // Turn on all Wifi logging
-	}
-	wifi.SetStandard (WIFI_PHY_STANDARD_80211g);
+  NodeContainer c;
+  c.Create (2);
 
-	YansWifiPhyHelper wifiPhy =  YansWifiPhyHelper::Default ();
-	// This is one parameter that matters when using FixedRssLossModel
-	// set it to zero; otherwise, gain will be added
-	wifiPhy.Set ("RxGain", DoubleValue (0) );
-	// ns-3 supports RadioTap and Prism tracing extensions for 802.11b
-	wifiPhy.SetPcapDataLinkType (YansWifiPhyHelper::DLT_IEEE802_11_RADIO);
+  // The below set of helpers will help us to put together the wifi NICs we want
+  WifiHelper wifi;
+  if (verbose)
+  {
+    wifi.EnableLogComponents ();  // Turn on all Wifi logging
+  }
+  wifi.SetStandard (WIFI_PHY_STANDARD_80211g);
 
-	YansWifiChannelHelper wifiChannel;
-	wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
-	// The below FixedRssLossModel will cause the rss to be fixed regardless
-	// of the distance between the two stations, and the transmit power
-	wifiChannel.AddPropagationLoss ("ns3::JakesPropagationLossModel");
-	wifiPhy.SetChannel (wifiChannel.Create ());
+  YansWifiPhyHelper wifiPhy =  YansWifiPhyHelper::Default ();
+  // This is one parameter that matters when using FixedRssLossModel
+  // set it to zero; otherwise, gain will be added
+  wifiPhy.Set ("RxGain", DoubleValue (0) );
+  // ns-3 supports RadioTap and Prism tracing extensions for 802.11b
+  wifiPhy.SetPcapDataLinkType (YansWifiPhyHelper::DLT_IEEE802_11_RADIO);
 
-	// Add a non-QoS upper mac, and disable rate control
-	NqosWifiMacHelper wifiMac = NqosWifiMacHelper::Default ();
-	wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
-			"DataMode",StringValue (phyMode),
-			"ControlMode",StringValue (phyMode));
-	// Set it to adhoc mode
-	wifiMac.SetType ("ns3::AdhocWifiMac");
-	NetDeviceContainer devices = wifi.Install (wifiPhy, wifiMac, c);
+  YansWifiChannelHelper wifiChannel;
+  wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
+  // The below FixedRssLossModel will cause the rss to be fixed regardless
+  // of the distance between the two stations, and the transmit power
+  wifiChannel.AddPropagationLoss ("ns3::JakesPropagationLossModel");
+  wifiPhy.SetChannel (wifiChannel.Create ());
 
-	MobilityHelper mobility;
-	Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
-	positionAlloc->Add (Vector (0.0, 0.0, 0.0));
-	positionAlloc->Add (Vector (5.0, 0.0, 0.0)); //5 meters away
-	mobility.SetPositionAllocator (positionAlloc);
-	mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-	mobility.Install (c);
+  // Add a non-QoS upper mac, and disable rate control
+  NqosWifiMacHelper wifiMac = NqosWifiMacHelper::Default ();
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
+      "DataMode",StringValue (phyMode),
+      "ControlMode",StringValue (phyMode));
+  // Set it to adhoc mode
+  wifiMac.SetType ("ns3::AdhocWifiMac");
 
 
-	InternetStackHelper internet;
-	AodvHelper aodv;
-	internet.SetRoutingHelper(aodv);
-	internet.Install (c);
+  NetDeviceContainer devices = wifi.InstallCR (wifiPhy, wifiMac, c);
 
-	Ipv4AddressHelper ipv4;
-	NS_LOG_INFO ("Assign IP Addresses.");
-	ipv4.SetBase ("10.1.1.0", "255.255.255.0");
-	Ipv4InterfaceContainer i = ipv4.Assign (devices);
+  NetDeviceContainer devices_control;
+  for (uint32_t i=0; i<devices.GetN(); i=i+3) {
+    devices_control.Add(devices.Get(i));
+  }
 
-	//
-	// Create a BulkSendApplication and install it on node 0
-	//
-	uint16_t port = 9;  // well-known echo port number
+  for (int x=0; x<2; x++)
+    for (int i=0; i<3; i++) {
+      //Address add = c.Get(x)->GetDevice(0)->GetAddress();
+      //NS_LOG_UNCOND("before change: " << add);
+      //add = add +1;
+      //NS_LOG_UNCOND("after change: " << add);
 
+      NS_LOG_UNCOND( "node[" <<x<< "] address " << i << " is: " << c.Get(x)->GetDevice(i)->GetAddress());
+    }
 
-	BulkSendHelper source ("ns3::TcpSocketFactory",
-			InetSocketAddress (i.GetAddress (1), port));
-	// Set the amount of data to send in bytes.  Zero is unlimited.
-	source.SetAttribute ("MaxBytes", UintegerValue (5000));
-	ApplicationContainer sourceApps = source.Install (c.Get (0));
-	sourceApps.Start (Seconds (0.0));
-	sourceApps.Stop (Seconds (10.0));
-
-	//
-	// Create a PacketSinkApplication and install it on node 1
-	//
-	PacketSinkHelper sink ("ns3::TcpSocketFactory",
-			InetSocketAddress (Ipv4Address::GetAny (), port));
-	ApplicationContainer sinkApps = sink.Install (c.Get (1));
-	sinkApps.Start (Seconds (0.0));
-	sinkApps.Stop (Seconds (10.0));
+  MobilityHelper mobility;
+  Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
+  positionAlloc->Add (Vector (0.0, 0.0, 0.0));
+  positionAlloc->Add (Vector (5.0, 0.0, 0.0)); //5 meters away
+  mobility.SetPositionAllocator (positionAlloc);
+  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  mobility.Install (c);
 
 
-	// Tracing
-	wifiPhy.EnablePcap ("wifi-simple-adhoc", devices);
+  InternetStackHelper internet;
+  AodvHelper aodv;
+  internet.SetRoutingHelper(aodv);
+  internet.Install (c);
 
-	// Output what we are doing
-	NS_LOG_UNCOND ("Testing ");
+  Ipv4AddressHelper ipv4;
+  NS_LOG_INFO ("Assign IP Addresses.");
+  ipv4.SetBase ("10.1.1.0", "255.255.255.0");
+  //ip addresses are only assigned for control devices
+  Ipv4InterfaceContainer i = ipv4.Assign (devices_control);
 
-	Simulator::Stop (Seconds (10.0));
-	Simulator::Run ();
-	Simulator::Destroy ();
+  //
+  // Create a BulkSendApplication and install it on node 0
+  //
+  uint16_t port = 9;  // well-known echo port number
 
-	Ptr<PacketSink> sink1 = DynamicCast<PacketSink> (sinkApps.Get (0));
-	std::cout << "Total Bytes Received: " << sink1->GetTotalRx () << std::endl;
-	return 0;
+
+  BulkSendHelper source ("ns3::TcpSocketFactory",
+      InetSocketAddress (i.GetAddress (1), port));
+  // Set the amount of data to send in bytes.  Zero is unlimited.
+  source.SetAttribute ("MaxBytes", UintegerValue (5000));
+  ApplicationContainer sourceApps = source.Install (c.Get (0));
+  sourceApps.Start (Seconds (0.0));
+  sourceApps.Stop (Seconds (10.0));
+
+  //
+  // Create a PacketSinkApplication and install it on node 1
+  //
+  PacketSinkHelper sink ("ns3::TcpSocketFactory",
+      InetSocketAddress (Ipv4Address::GetAny (), port));
+  ApplicationContainer sinkApps = sink.Install (c.Get (1));
+  sinkApps.Start (Seconds (0.0));
+  sinkApps.Stop (Seconds (10.0));
+
+
+  // Tracing
+  wifiPhy.EnablePcap ("wifi-simple-adhoc", devices_control);
+
+  // Output what we are doing
+  NS_LOG_UNCOND ("Testing ");
+
+  //sinkApps.Get(0)->TraceConnectWithoutContext("Rx", MakeCallback(&RxRcv));
+  Config::Connect("/NodeList/*/ApplicationList/*/$ns3::PacketSink/Rx", MakeCallback(&RxRcv));
+
+  Simulator::Stop (Seconds (10.0));
+  Simulator::Run ();
+  Simulator::Destroy ();
+
+  Ptr<PacketSink> sink1 = DynamicCast<PacketSink> (sinkApps.Get (0));
+  std::cout << "Total Bytes Received: " << sink1->GetTotalRx () << std::endl;
+  //TracedCallback<Ptr<const Packet>, const Address &> m_rxTrace;
+  //sink1->TraceConnectWithoutContext("Rx", MakeCallback(&RxRcv));
+
+  return 0;
 }
 
